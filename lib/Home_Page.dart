3c21974c5e6d799a1ffe8270df5698bb3705254d
Tallha khan <tallha.khan0302@gmail.com>
2022-06-07 02:59:45 +0500

    Day2 Completed

---
+++ lib/Home_Page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String appName = "Catalog App";
+    String todayDate = "07/06/2022";
+
+    return Scaffold(
+      appBar: AppBar(title: Text(appName)),
+      body: Center(
+        child: Container(
+          child: Text("My First try App $todayDate"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/Home_Page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-          child: Center(
-        child: Container(
-          child: const Text("My First try App"),
-        ),
-      )),
+      home: Home_Page(),
     );
   }
 }
