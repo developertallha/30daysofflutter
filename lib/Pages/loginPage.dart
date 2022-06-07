b51c8efb0f87524b7b58ad313db9121cb706bb8b
Tallha khan <tallha.khan0302@gmail.com>
2022-06-08 02:06:32 +0500

    Day4Completed

--- lib/Pages/loginPage.dart
+++ lib/Pages/loginPage.dart
@@ -6,17 +6,65 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Container(
-          child: const Text("Hello 123",
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_img.png", fit: BoxFit.cover),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
               style: TextStyle(
-                fontSize: 40,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Colors.amber,
-                backgroundColor: Colors.black,
-              )),
-        ),
-      ),
-    );
+                letterSpacing: 1.0,
+              ),
+            ),
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter your username',
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter your Password',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      child: Text('Sign in'),
+                      onPressed: () {
+                        print("im runing");
+                      },
+                      style: ElevatedButton.styleFrom(
+                          side: BorderSide(width: 3, color: Colors.amberAccent),
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(
+                              //to set border radius to button
+                              borderRadius: BorderRadius.circular(30)),
+                          primary: Colors.white,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 20),
+                          textStyle: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )),
+                    ),
+                  ],
+                )),
+          ],
+        ));
   }
 }
