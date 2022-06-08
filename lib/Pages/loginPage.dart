3481f56a32e03db2366692697ea178dcb4d2ed6b
Tallha khan <tallha.khan0302@gmail.com>
2022-06-08 09:57:59 +0500

    Day5 Completed

--- lib/Pages/loginPage.dart
+++ lib/Pages/loginPage.dart
@@ -1,70 +1,75 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_catalog/Utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset("assets/images/login_img.png", fit: BoxFit.cover),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.0,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/images/login_img.png", fit: BoxFit.contain),
+              SizedBox(
+                height: 20.0,
               ),
-            ),
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your username',
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0,
+                ),
+              ),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Enter your username',
+                        ),
                       ),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your Password',
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Enter your Password',
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      child: Text('Sign in'),
-                      onPressed: () {
-                        print("im runing");
-                      },
-                      style: ElevatedButton.styleFrom(
-                          side: BorderSide(width: 3, color: Colors.amberAccent),
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                              //to set border radius to button
-                              borderRadius: BorderRadius.circular(30)),
-                          primary: Colors.white,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 50, vertical: 20),
-                          textStyle: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
-                    ),
-                  ],
-                )),
-          ],
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        child: Text('Sign in'),
+                        onPressed: () {
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            side:
+                                BorderSide(width: 3, color: Colors.amberAccent),
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                                //to set border radius to button
+                                borderRadius: BorderRadius.circular(30)),
+                            primary: Colors.white,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 20),
+                            textStyle: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )),
+                      ),
+                    ],
+                  )),
+            ],
+          ),
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/Home_Page.dart';
 import 'package:flutter_catalog/Pages/loginPage.dart';
+import 'package:flutter_catalog/Utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.cyan,
@@ -19,8 +21,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => Home_Page(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Home_Page(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
